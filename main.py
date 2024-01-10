47eee0eb7abee2608228c87c70306fb99e9d4123
EZZAHRI Oussama <ezzahrioussama01@gmail.com>
2024-01-10 23:27:37 +0100

    Update main file

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-import face_recognition
 import cv2
+import os
 
-# Load the reference image for facial recognition
-known_image = face_recognition.load_image_file("img.jpg")
-known_encoding = face_recognition.face_encodings(known_image)[0]
+cap = cv2.VideoCapture(0)
 
-# Video capture using OpenCV
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+folderPath = 'Resources/Modes'
+modePathList = os.listdir(folderPath)
+imgModeList = []
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderPath, path)))
+print(len(imgModeList))
+
+
 
 while True:
-    ret, frame = cap.read()
-
-    # Display the current frame
-    cv2.imshow('Camera', frame)
-
-
-    # Check for the spacebar key press
-    key = cv2.waitKey(1)
-    if key == ord(' '):
-        # Find faces in the current frame
-        face_locations = face_recognition.face_locations(frame)
-        face_encodings = face_recognition.face_encodings(frame, face_locations)
-
-        # Process each detected face
-        for face_encoding in face_encodings:
-            # Compare the current face with the known face
-            match = face_recognition.compare_faces([known_encoding], face_encoding)
-
-            if match[0]:
-                print("Face recognized!")
-
-    # Check for the 'q' key press to exit the loop
-    elif key == ord('q'):
-        break
-
-# Release the video capture and close the window
-cap.release()
-cv2.destroyAllWindows()
+    success, img = cap.read()
+    imgBackground[162:162+480, 55:55+640] = img
+    imgBackground[44:44+633, 808:808+414] = imgModeList[0]
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Student Attendance", imgBackground)
+    cv2.waitKey(1)
