5617d4f653760779a9fa7de723dcb8e58e93b8fd
EZZAHRI Oussama <ezzahrioussama01@gmail.com>
2024-01-22 14:40:27 +0100

    install icons

---
+++ dist/PHP/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../output.css">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Slide bar</title>
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="navigation">
+            <ul>
+                <li class="mb-2">
+                    <a href="#"class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="logo-microsoft"></ion-icon>
+                        </span>
+                        <span class="title">OS Brand</span>
+                    </a>
+
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Home</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="school-outline"></ion-icon>
+                        </span>
+                        <span class="title">Students</span>
+                    </a>
+                </li>
+
+                
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="school-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="school-outline"></ion-icon>
+                        </span>
+                        <span class="title">Users</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="alert-outline"></ion-icon>
+                        </span>
+                        <span class="title">Attendance</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Users</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="download-outline"></ion-icon>
+                        </span>
+                        <span class="title">Export</span>
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-gray-300">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Logout</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
