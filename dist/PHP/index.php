f8d83ad7969703272364b1073a9192333f58bc23
EZZAHRI Oussama <ezzahrioussama01@gmail.com>
2024-01-23 21:48:13 +0100

    Add admin dashboard

--- dist/PHP/index.php
+++ dist/PHP/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../output.css">
     <link rel="stylesheet" href="../CSS/style.css">
+    <!-- <link rel="stylesheet" href="../CSS/admin.css"> -->
     <title>Slide bar</title>
 
 </head>
@@ -22,14 +23,19 @@
                         <span class="title">OS Brand</span>
                     </a>
                 </li>
+
+
                 <li class="mb-2 item">
                     <a href="#" class="">
                         <span class="icon">
-                            <ion-icon name="home-outline"></ion-icon>
+                            <ion-icon name="analytics-outline"></ion-icon>
                         </span>
-                        <span class="title">Home</span>
+                        <span class="title">Dashboard</span>
                     </a>
                 </li>
+
+
+
                 <li class="mb-2 item">
                     <a href="#" class="">
                         <span class="icon">
@@ -39,15 +45,6 @@
                     </a>
                 </li>
 
-
-                <li class="mb-2 item">
-                    <a href="#" class="">
-                        <span class="icon">
-                            <ion-icon name="school-outline"></ion-icon>
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
 
                 <li class="mb-2 item">
                     <a href="#" class="">
@@ -93,15 +90,74 @@
                 <div class="toggle text-gray-800 cursor-pointer">
                     <ion-icon name="menu-outline" class="text-3xl"></ion-icon>
                 </div>
+
                 <div class="admin w-14 h-14 overflow-hidden rounded-full cursor-pointer">
                     <img src="../pictures/student5651.jpg" alt="profile picture" class="w-full h-full object-cover">
                 </div>
             </div>
-        </div>
 
+
+
+            <?php include('config.php'); ?>
+
+            <section class="dashboard">
+                <div class="text-center">
+                    <span class="text-5xl text-gray-800 font-semibold">TABLEAU <span class="text-blue-700">DE BORD</span></span>
+                </div>
+
+                <div class="box-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6 max-w-6xl mx-auto mt-8 ">
+
+                    <a href="#" class="box border rounded-md p-8 bg-white shadow-md text-center">
+                        <?php
+                        $select_account = mysqli_query($conn, "SELECT * FROM `students`") or die('query failed');
+                        $number_of_students = mysqli_num_rows($select_account);
+                        ?>
+                        <h3 class="text-5xl font-bold text-blue-700"><?php echo $number_of_students; ?></h3>
+                        <p class="mt-6 p-6 bg-gray-100 text-gray-800 text-2xl rounded border">Totals des etudiats</p>
+                    </a>
+
+                    <a href="#" class="box border rounded-md p-8 bg-white shadow-md text-center">
+                        <?php
+                        $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+                        $number_of_account = mysqli_num_rows($select_account);
+                        ?>
+                        <h3 class="text-5xl font-bold text-blue-700"><?php echo $number_of_account; ?></h3>
+                        <p class="mt-6 p-6 bg-gray-100 text-gray-800 text-2xl rounded border">Totals des comptes</p>
+                    </a>
+
+                    <a href="#" class="box border rounded-md p-8 bg-white shadow-md text-center">
+                        <?php
+                        $select_account = mysqli_query($conn, "SELECT * FROM `attendancerecords`") or die('query failed');
+                        $number_of_attendances = mysqli_num_rows($select_account);
+                        ?>
+                        <h3 class="text-5xl font-bold text-blue-700"><?php echo $number_of_attendances; ?></h3>
+                        <p class="mt-6 p-6 bg-gray-100 text-gray-800 text-2xl rounded border">Totals des abseneces</p>
+                    </a>
+                    <a href="#" class="box border rounded-md p-8 bg-white shadow-md text-center">
+                        <?php
+                        $select_account = mysqli_query($conn, "SELECT * FROM `attendancerecords`") or die('query failed');
+                        $number_of_attendances = mysqli_num_rows($select_account);
+                        ?>
+                        <h3 class="text-5xl font-bold text-blue-700"><?php echo $number_of_attendances; ?></h3>
+                        <p class="mt-6 p-6 bg-gray-100 text-gray-800 text-2xl rounded border">Totals des abseneces</p>
+                    </a>
+
+
+                    <!-- Repeat the box structure for additional items -->
+
+                </div>
+            </section>
+
+        </div>
 
     </div>
 
+    <script>
+        function handleBoxClick(boxType) {
+            alert("Clicked on " + boxType);
+
+        }
+    </script>
 
 
 
